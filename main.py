45210a2034fff8e84c485240fffc33717aae0bf4
TheGorick <thegorick@gmail.com>
2025-01-29 14:30:35 -0500

    Almost done

--- main.py
+++ main.py
@@ -2,29 +2,6 @@
 #Félix-Antonin Noël
 #Guillaume Pinat
 
-"""
-Classe abonnés:
-Attributs:
-identifiant
-Prénom
-Nom
------
-Classe Bibliothèque
-Attributs:
-
-
-Méthodes:
-Ajouter abonné
-Supprimer abonné
-Afficher tout abonné
-ajouter document
-Supprimer document
-Afficher tout document
-
-Classe bibliothèque
-Classe Documents
-
-"""
 import Menu as Menu
 from PyQt6.QtWidgets import QApplication
 import sys
